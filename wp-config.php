240fa148b6de0ff957afcdcfa131456282c563cb
karkisarkar <prabin@nydoz.com>
2024-05-15 14:50:08 +0545

    real

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'gowithfund' );
+define( 'DB_NAME', 'u999709570_hP1jz' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u999709570_GlHDZ' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'e#591#7Z' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
